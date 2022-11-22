1420202b05137f2c8a4eefdcc7f9366d46913351
nikita <nikitin.ninise@gmail.com>
2022-11-22 16:33:57 +0200

    Initial Commit

---
+++ Beautiful Weather App/Beautiful_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beautiful_Weather_AppApp.swift
+//  Beautiful Weather App
+//
+//  Created by Nikita on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Beautiful_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
